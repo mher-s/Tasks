595556ffd39adf778f3160075c2f77d6b6624c2e
Mher <mhersargsyan2001@gmail.com>
2021-04-28 17:46:23 +0400

    feat: users import nums task finished

---
+++ lesson-2/js/usersNums.js
@@ -0,0 +1,6 @@
+alert("Please, enter 2 numbers i will return you their sum");
+let firstNum = +prompt("Enter first number");
+let secondNum = +prompt("Enter second number");
+
+let sum = firstNum + secondNum;
+alert(sum);
