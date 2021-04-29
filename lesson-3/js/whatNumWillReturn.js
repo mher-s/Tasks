2d0955b54fe1c415e868190adff2e729e8828802
Mher <mhersargsyan2001@gmail.com>
2021-04-29 17:47:44 +0400

    what num will return

---
+++ lesson-3/js/whatNumWillReturn.js
@@ -0,0 +1,15 @@
+let i = 0;
+
+//             1 2 3 4 5
+
+while (++i < 5) alert(i);
+
+// will return 1 2 3 4
+
+
+let i = 0;
+//             0 1 2 3 4 5
+
+while (i++ < 5) alert(i);
+
+// will return 1 2 3 4 5
