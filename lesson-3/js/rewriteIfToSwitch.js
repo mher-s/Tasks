68f217381d72122d865600c099b511a2f23474c6
Mher <mhersargsyan2001@gmail.com>
2021-04-29 17:34:14 +0400

    feat: rewrite if to switch or switch to if

---
+++ lesson-3/js/rewriteIfToSwitch.js
@@ -0,0 +1,27 @@
+let browser = "Bla-bla";
+
+if (browser === "Edge") {
+  console.log("You've got the Edge!");
+} else if (chrome || firefox || opera || safari) {
+  console.log("Okay we support these browsers too");
+} else {
+  console.log("We hope that this page looks ok!");
+}
+
+//////////////////////////////
+
+switch (a) {
+  case a === 0:
+    alert(0);
+    break;
+  case a === 1:
+    alert(1);
+    break;
+  case a === 2:
+  case a === 3:
+    alert('2,3');
+    break;
+
+  default:
+    break;
+}
