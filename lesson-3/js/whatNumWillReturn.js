197d8769743c35d7a3ea7e597ff28129454a7f3e
Mher <mhersargsyan2001@gmail.com>
2021-04-29 17:49:47 +0400

    feat: what num will return more

--- lesson-3/js/whatNumWillReturn.js
+++ lesson-3/js/whatNumWillReturn.js
@@ -13,3 +13,11 @@ let i = 0;
 while (i++ < 5) alert(i);
 
 // will return 1 2 3 4 5
+
+/////////////////////////////////////
+
+for (let i = 0; i < 5; i++) alert(i);
+
+for (let i = 0; i < 5; ++i) alert(i);
+
+// both will return 4 cuz start from 0
