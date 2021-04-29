77df8a37ddc1389d0bf665281424703fd9094cdf
Mher <mhersargsyan2001@gmail.com>
2021-04-29 13:26:19 +0400

    feat: odd or even task finished

---
+++ lesson-3/js/oddOrEven.js
@@ -0,0 +1,10 @@
+function oddOrEven(a) {
+  if (a === 0) {
+    console.log("Who know's");
+  } else if (a % 2 === 0) {
+    console.log("Even");
+  } else {
+    console.log("odd");
+  }
+}
+oddOrEven(4);
