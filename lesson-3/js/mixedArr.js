6696af3a9669671d271ef1216a4f0d5dfeea4277
Mher <mhersargsyan2001@gmail.com>
2021-04-30 11:35:51 +0400

    fixed: mixed array bug fixed

--- lesson-3/js/mixedArr.js
+++ lesson-3/js/mixedArr.js
@@ -1,8 +1,9 @@
 let array = [8, 0, 1, "hey", 12, 5, true, "2", null, 7, 3];
+let arr = []
 
 for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] !== 'number') {
-        delete array[i]
+    if (typeof array[i] === 'number') {
+        arr.push(array[i])
     }    
 }
-console.log(array)
+console.log(arr)
