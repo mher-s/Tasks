d48ae77c2e5061d96a29385637e2be9acfca052b
Mher <mhersargsyan2001@gmail.com>
2021-04-30 12:05:51 +0400

    new task: count all words length

---
+++ lesson-3/js/countLength.js
@@ -0,0 +1,9 @@
+let array = ["anymore", "raven", "me", "communicate"];
+let count = 0;
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array[i].length; j++) {
+    count++
+  }
+}
+console.log(count)
