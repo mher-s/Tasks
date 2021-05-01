2f791874faafb7ab16058282ee30af04ed2a760e
Mher <mhersargsyan2001@gmail.com>
2021-05-01 16:25:40 +0400

    fix: form validation task

--- lesson-3/optionalTaskJs/inputValidation.js
+++ lesson-3/optionalTaskJs/inputValidation.js
@@ -1,9 +1,17 @@
 let userInput = prompt("Enter: ");
 
 let numbers = /[0-9]/g;
-let letters = /[A-Z a-z]/g;
+let lettersUpper = /[A-Z]/g;
+let lettersLower = /[a-z]/g;
 let symbols = /[~,!,@,#,$,%,^,&,*,(,),_,+]/g;
 
-if (userInput === numbers) {
+if (
+  userInput.match(numbers) &&
+  userInput.match(lettersUpper) &&
+  userInput.match(lettersUpper) &&
+  userInput.match(symbols)
+) {
   console.log("valid");
+} else {
+  console.log("Invalid");
 }
