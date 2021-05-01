8e6733a302d1b6f5dba20f80749db8e35db4f686
Mher <mhersargsyan2001@gmail.com>
2021-05-01 11:28:28 +0400

    new task: make triangle task, need ask

---
+++ lesson-3/js/makeTriangle.js
@@ -0,0 +1,12 @@
+let size = 5;
+
+let result = "";
+for (let i = 1; i <= size; i++) {
+  for (let j = 1, m = i; j <= i; j++) {
+    result += ` ${m}`;
+    m += size - j;
+  }
+  result += "\n";
+}
+
+console.log(result);
