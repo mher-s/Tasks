0f73f30c853e02b48a3d3f0ca4109144c454da36
Mher <mhersargsyan2001@gmail.com>
2021-05-02 11:43:40 +0400

    new task: repeat task

---
+++ lesson-3/lodashTaskJs/repeat.js
@@ -0,0 +1,12 @@
+// let str = '*'
+// let repeatStr = str.repeat(3)
+// console.log(repeatStr)
+
+let needRepeat = "*";
+let countRepeat = 23;
+let repeatOutput = "";
+
+for (let i = 0; i < countRepeat; i++) {
+  repeatOutput += needRepeat;
+}
+console.log(repeatOutput);
