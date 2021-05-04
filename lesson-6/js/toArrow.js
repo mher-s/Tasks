354bc921dda3a798e6e3de0ca7048bafbad1d969
Mher <mhersargsyan2001@gmail.com>
2021-05-04 13:48:33 +0400

    feat: to arrow

---
+++ lesson-6/js/toArrow.js
@@ -0,0 +1,22 @@
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "Do you agree?",
+//   function() { alert("You agreed."); },
+//   function() { alert("You canceled the execution."); }
+//
+// Need rewrite
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
