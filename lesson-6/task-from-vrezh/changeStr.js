b1f20b21bdabce68ccca32f06e90aa86fb66936b
Mher <mhersargsyan2001@gmail.com>
2021-05-04 19:10:10 +0400

    feat: delete from str

---
+++ lesson-6/task-from-vrezh/changeStr.js
@@ -0,0 +1,8 @@
+function deleteStr(sentence, str) {
+  while (sentence.includes(str)) {
+    sentence = sentence.replace(str, "");
+  }
+  return sentence;
+}
+
+console.log(deleteStr("alax is akbar", "is"));
