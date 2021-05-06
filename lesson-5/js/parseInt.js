ecfe9261280e474a8ff34f6a8aecb9ac71bcff83
Mher <mhersargsyan2001@gmail.com>
2021-05-06 23:49:04 +0400

    fix: parse int function fixed

--- lesson-5/js/parseInt.js
+++ lesson-5/js/parseInt.js
@@ -1,11 +1,24 @@
-let userInput = prompt("Enter: ");
+function myParseInt(str) {
+  str = str.trim();
+  let result = "";
+  let sign = "";
 
-for (let i = 0; i < userInput.length; i++) {
-  if (!isNaN(userInput[i] && userInput[i] === ".")) {
-    console.log(Number(Math.floor(userInput)));
-    break;
-  } else {
-    console.log(NaN);
-    break;
+  if (str.charAt(0) === "-") {
+    sign = str.slice(0, 1);
+    str = str.slice(1);
   }
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      result = result += str[i];
+    } else if (result.length) {
+      return Number(sign + result);
+    } else {
+      return NaN;
+    }
+  }
+  return sign + result;
 }
+
+console.log(myParseInt("1231"));
+
