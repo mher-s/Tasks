e541f4be153fbc929ccd18c9bd742a3db303988f
Mher <mhersargsyan2001@gmail.com>
2021-05-11 19:28:44 +0400

    feat: my for each

---
+++ lesson-10/implement/myForEach.js
@@ -0,0 +1,10 @@
+function myForEach(param) {
+  for (let i = 0; i < param.length; i++) {
+    const element = param[i];
+    // console.log(i);
+    // console.log(element)
+    // console.log(param);
+  }
+}
+
+myForEach(["q", "w", "e", "r", "t", "y"]);
