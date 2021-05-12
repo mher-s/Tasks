bc63f66752cad08b74404610337cf8c1695e84cb
Mher <mhersargsyan2001@gmail.com>
2021-05-12 14:03:52 +0400

    fix: find

--- lesson-10/implement/find.js
+++ lesson-10/implement/find.js
@@ -1,4 +1,4 @@
-function myFindIndex(arr, fn) {
+function myFind(arr, fn) {
   for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
     if (fn(element)) {
@@ -9,7 +9,7 @@ function myFindIndex(arr, fn) {
 }
 
 console.log(
-  myFindIndex([1, 2, 3, 4, 5, 6, 7, 8, 9], (element) => {
+  myFind([1, 2, 3, 4, 5, 6, 7, 8, 9], (element) => {
     return element > 5;
   })
 );
