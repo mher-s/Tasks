1611ea7a7ebf6cb4229a14ab886401bf6e570dc6
Mher <mhersargsyan2001@gmail.com>
2021-05-12 18:35:28 +0400

    fix: add null

--- lesson-10/task-from-vrezh/addNull.js
+++ lesson-10/task-from-vrezh/addNull.js
@@ -1,7 +1,7 @@
 function addNullIfNaN(arr) {
   let result = [];
   arr.map(function checkNumbers(arr) {
-    if (!isNaN(arr)) {
+    if (parseInt(arr)) {
       result.push(arr);
     } else {
       result.push(null);
@@ -10,4 +10,21 @@ function addNullIfNaN(arr) {
   return result;
 }
 
-console.log(addNullIfNaN([1, undefined, 3, NaN, 5, null, 7, 'str', 9, [1,2,3], 11, ['str', 'str', 'str']]));
+console.log(
+  addNullIfNaN([
+    1,
+    undefined,
+    3,
+    NaN,
+    5,
+    null,
+    7,
+    "str",
+    9,
+    [1, 2, 3],
+    11,
+    ["str", "str", "str"],
+    true,
+    false,
+  ])
+);
