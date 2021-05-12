5f48993b02d69dd89869c0c1ac8a7c90aec2f4b0
Mher <mhersargsyan2001@gmail.com>
2021-05-12 14:02:31 +0400

    feat: find

---
+++ lesson-10/implement/find.js
@@ -0,0 +1,15 @@
+function myFindIndex(arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (fn(element)) {
+      return element;
+    }
+  }
+  return thatIndex;
+}
+
+console.log(
+  myFindIndex([1, 2, 3, 4, 5, 6, 7, 8, 9], (element) => {
+    return element > 5;
+  })
+);
