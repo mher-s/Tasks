9c7a15bf8adc00731b821eea79f8fab696a5fdd5
Mher <mhersargsyan2001@gmail.com>
2021-05-15 15:27:32 +0400

    feat: from min to max

---
+++ edabit-task/sortingAlgorith.js
@@ -0,0 +1,15 @@
+function sortFromMinToMax(arr) {
+  let temp = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortFromMinToMax([2, 1, 4, 5, 3, 8, 9, 7, 6]));
