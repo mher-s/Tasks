78d456a0238b4217f13e73498c9ac9fe921758fd
Mher <mhersargsyan2001@gmail.com>
2021-05-23 21:17:13 +0400

    feat: person

---
+++ lesson-16/person.js
@@ -0,0 +1,25 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+  this.eat = function eat() {
+    if (stomach.length === 10) {
+      return `Stomach is full`;
+    } else {
+      this.stomach.push("food");
+    }
+  };
+  this.poop = function poop() {
+    this.stomach.pop("food");
+  };
+  this.greeting = function greeting() {
+    return `hey my name is ${this.name}, I am ${this.age} y.o. what about u`;
+  };
+
+  this.getNameAndAge = function () {
+    return `${this.name},${this.age}`;
+  };
+}
+
+const p1 = new Person("Vzgo", 20);
+console.log(p1.eat());
