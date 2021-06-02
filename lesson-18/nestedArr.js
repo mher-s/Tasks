d37537388511c491617b9ea839e0e0b55869f657
Mher <mhersargsyan2001@gmail.com>
2021-06-02 19:22:54 +0400

    feat: nested arr

---
+++ lesson-18/nestedArr.js
@@ -0,0 +1,13 @@
+function nestedArr(arr, idx = 0) {
+  if (Array.isArray(arr[idx])) {
+    arr.splice(idx, 1, ...arr[idx]);
+    idx -= 1;
+  }
+  if (idx === arr.length - 1) {
+    return arr;
+  }
+  return nestedArr(arr, (idx += 1));
+}
+
+console.log(nestedArr([1, [3, 4, [1, 2]], 10]));
+console.log(nestedArr([14, [1, [[[3, []]], 1], 0]]));
