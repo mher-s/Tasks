4629c3da8e3ea91ec11b0c1b26a21f70d46f9fb4
Mher <mhersargsyan2001@gmail.com>
2021-06-02 19:27:42 +0400

    fix: nested arr little fix

--- lesson-18/nestedArr.js
+++ lesson-18/nestedArr.js
@@ -1,9 +1,9 @@
 function nestedArr(arr, idx = 0) {
   if (Array.isArray(arr[idx])) {
     arr.splice(idx, 1, ...arr[idx]);
-    idx -= 1;
+      idx -= 1;
   }
-  if (idx === arr.length - 1) {
+  if (idx === arr.length) {
     return arr;
   }
   return nestedArr(arr, (idx += 1));
