18421ffdd1d2daa9c5b48c4c99ba2a0d7311767f
Mher <mhersargsyan2001@gmail.com>
2021-06-22 18:29:27 +0400

    feat(edabit): basketball point counter

---
+++ edabit-task/basketballPoints.js
@@ -0,0 +1,7 @@
+function points(twoPoint, threePoint) {
+    return twoPoint*2 + threePoint*3
+}
+
+console.log(points(1, 1)) // ➞ 5
+console.log(points(7, 5)) // ➞ 29
+console.log(points(38, 8)) // ➞ 100
