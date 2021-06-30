c998b7f645e7b10b4cb9d7090f615fb0aa3aa67f
Mher <mhersargsyan2001@gmail.com>
2021-06-30 09:11:08 +0400

    feat(js-tutorial): variables

---
+++ JS-Tutorial/variables.js
@@ -0,0 +1,18 @@
+//  Task 1
+
+let admin;
+let name = "John";
+
+admin = name;
+
+console.log(admin)
+
+// Task 2
+
+let myPlanet;
+let currentUser;
+
+// Task 3
+
+const birthdayDate = '19.04.1980'
+const age = '19'
