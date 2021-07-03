3819f4b64ab225e31500d35a434e0ffe32224ae1
Mher <mhersargsyan2001@gmail.com>
2021-07-03 19:44:13 +0400

    feat(js-tutorial): operator

--- JS-Tutorial/operators.js
+++ JS-Tutorial/operators.js
@@ -1,8 +1,14 @@
-let a = 1,
-  b = 1;
+// let a = 1,
+//   b = 1;
 
-let c = ++a; // 2
-let d = b++; // 1
+// let c = ++a; // 2
+// let d = b++; // 1
 
-console.log(c)
-console.log(d)
+// console.log(c)
+// console.log(d)
+
+let a = 2;
+
+let x = 1 + (a *= 2); // 5
+
+console.log(x)
