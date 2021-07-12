4c6a1953c2ff37ea862d6615ba1934f1614171ba
Mher <mhersargsyan2001@gmail.com>
2021-07-12 09:02:12 +0400

    feat(js-tutorial/prototype): hamster inheritance

--- JS-Tutorial/prototype.js
+++ JS-Tutorial/prototype.js
@@ -1,47 +1,72 @@
-// // let animal = {
-// //   jumps: null,
+// // // let animal = {
+// // //   jumps: null,
+// // // };
+// // // let rabbit = {
+// // //   __proto__: animal,
+// // //   jumps: true,
+// // // };
+
+// // // alert(rabbit.jumps); // true (1)
+// // // delete rabbit.jumps;
+
+// // // alert(rabbit.jumps); // null (2)
+// // // delete animal.jumps;
+
+// // // alert(rabbit.jumps); // undefined (3)
+
+// // let head = {
+// //   glasses: 1,
 // // };
-// // let rabbit = {
-// //   __proto__: animal,
-// //   jumps: true,
+
+// // let table = {
+// //   pen: 3,
+// //   __proto__: head,
 // // };
 
-// // alert(rabbit.jumps); // true (1)
-// // delete rabbit.jumps;
+// // let bed = {
+// //   sheet: 1,
+// //   pillow: 2,
+// //   __proto__: table,
+// // };
 
-// // alert(rabbit.jumps); // null (2)
-// // delete animal.jumps;
+// // let pockets = {
+// //   money: 2000,
+// //   __proto__: bed,
+// // };
 
-// // alert(rabbit.jumps); // undefined (3)
-
-// let head = {
-//   glasses: 1,
+// let animal = {
+//   eat() {
+//     this.full = true;
+//   },
 // };
 
-// let table = {
-//   pen: 3,
-//   __proto__: head,
+// let rabbit = {
+//   __proto__: animal,
 // };
 
-// let bed = {
-//   sheet: 1,
-//   pillow: 2,
-//   __proto__: table,
-// };
+// // answer rabbit
 
-// let pockets = {
-//   money: 2000,
-//   __proto__: bed,
-// };
-
-let animal = {
-  eat() {
-    this.full = true;
+let hamster = {
+  eat(food) {
+    this.stomach.push(food);
   },
 };
 
-let rabbit = {
-  __proto__: animal,
+let speedy = {
+  stomach: [],
+  __proto__: hamster,
 };
 
-// answer rabbit
+let lazy = {
+  stomach: [],
+  __proto__: hamster,
+};
+
+// This one found the food
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// This one also has it, why? fix please.
+console.log(lazy.stomach); // apple
+
+// both hamster inherit from hamster and stomach cant be same for both hamsters
